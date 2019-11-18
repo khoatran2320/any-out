b757e975bf667f4c7447f0422bb3a3877d857d22
Khoa Tran <kttran2320@gmail.com>
2019-11-17 20:24:00 -0500

    more code

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def home():
     return render_template("home.html")
-
+@app.route("/login", methods=["GET", "POST"])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4000, debug=True)
