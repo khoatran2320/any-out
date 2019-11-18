ae7975ae0b589657fad0a8b69e22fe4635fcf308
Khoa Tran <kttran2320@gmail.com>
2019-11-18 13:06:56 -0500

    set up mongoengine

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 
-
+from mongoengine import connect
 from flask import Flask, render_template, request
-
+db = connect('anyout')
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"])
 def home():
+    
     return render_template("home.html")
-@app.route("/login", methods=["GET", "POST"])
+# @app.route("/login", methods=["GET", "POST"])
 
-@app.route("/signup", methods=["GET", "POST"])
+# @app.route("/signup", methods=["GET", "POST"])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4000, debug=True)
