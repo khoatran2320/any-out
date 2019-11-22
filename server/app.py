76e1db6d2117cde0fa563172b9e4fdf6a48561fe
abdelaziz <ahussein@bu.edu>
2019-11-21 23:19:59 -0500

    added the sign up backend, still needs work. how do we import the hashing function if they are not in the same folder? I just put a # to comment it out for now

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@ from bson import Binary, Code
 from flask import Flask, render_template, request, jsonify
 from flask_pymongo import PyMongo
 from flask_cors import CORS
+#from AuthenticationService.py import hash_Password(password)
 
 app = Flask(__name__)
 CORS(app)
@@ -20,6 +21,24 @@ def home():
 # # login harry, compare credentials to database
 # @app.route("/signup", methods=["GET", "POST"])
 # # sign abdel save credentials to database, hash password
+
+@app.route('/users/register', methods=['POST'])
+def register():
+users = mongo.db.users
+first_name = request.get_json()['first_name']
+last_name = request.get_json()['last_name']
+email = request.get_json()['email']
+password = hash_Password(request.get_json()['password'])
+
+
+user_id = users.insert({
+'first_name' :first_name,
+'last_name': last_name,
+'email' :email,
+'password':password,
+})
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4000, debug=True)
